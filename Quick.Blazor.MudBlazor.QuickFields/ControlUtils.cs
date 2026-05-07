bc1548d3b71d5c2eabb3c295c0e8fd00f45abe41
张鹏 <scbeta@qq.com>
2026-05-07 15:54:59 +0800

    First commit.

---
+++ Quick.Blazor.MudBlazor.QuickFields/ControlUtils.cs
@@ -0,0 +1,76 @@
+using System.Text;
+using MudBlazor;
+using Quick.Fields;
+
+namespace Quick.Blazor.MudBlazor.QuickFields;
+
+internal static class ControlUtils
+{
+    private static void appendCommonClass(StringBuilder sb, FieldForGet field)
+    {
+        if (field.Margin.HasValue)
+            sb.Append(" m-" + field.Margin.Value);
+        if (field.MarginLeft.HasValue)
+            sb.Append(" ml-" + field.MarginLeft.Value);
+        if (field.MarginTop.HasValue)
+            sb.Append(" mt-" + field.MarginTop.Value);
+        if (field.MarginRight.HasValue)
+            sb.Append(" mr-" + field.MarginRight.Value);
+        if (field.MarginBottom.HasValue)
+            sb.Append(" mb-" + field.MarginBottom.Value);
+
+        if (field.Padding.HasValue)
+            sb.Append(" p-" + field.Padding.Value);
+        if (field.PaddingLeft.HasValue)
+            sb.Append(" pl-" + field.PaddingLeft.Value);
+        if (field.PaddingTop.HasValue)
+            sb.Append(" pt-" + field.PaddingTop.Value);
+        if (field.PaddingRight.HasValue)
+            sb.Append(" pr-" + field.PaddingRight.Value);
+        if (field.PaddingBottom.HasValue)
+            sb.Append(" pb-" + field.PaddingBottom.Value);
+
+        if (field.ColumnWidth.HasValue)
+        {
+            if (field.ColumnWidth.Value <= 0)
+                sb.Append(" col");
+            else
+                sb.Append(" col-" + field.ColumnWidth.Value);
+        }
+    }
+
+    public static string GetCommonClass(FieldForGet field, string baseClass = null)
+    {
+        if (!string.IsNullOrEmpty(field.Html_Class))
+            return field.Html_Class;
+        var sb = new StringBuilder();
+        if (!string.IsNullOrEmpty(baseClass))
+            sb.Append(baseClass);
+        appendCommonClass(sb, field);
+        return sb.ToString();
+    }
+    
+    public static bool GetInputReadOnly(FieldForGet field)
+    {
+        if (field.Input_ReadOnly == null)
+            return false;
+        return field.Input_ReadOnly.Value;
+    }
+
+    public static Severity GetSeverity(FieldForGet field)
+    {
+        switch (field.Theme)
+        {
+            case FieldTheme.Warning:
+                return Severity.Warning;
+            case FieldTheme.Info:
+                return Severity.Info;
+            case FieldTheme.Success:
+                return Severity.Success;
+            case FieldTheme.Danger:
+                return Severity.Error;
+            default:
+                return Severity.Normal;
+        }
+    }
+}
---
+++ Quick.Blazor.MudBlazor.QuickFields/Controls.razor.cs
@@ -0,0 +1,97 @@
+using System.ComponentModel;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Quick.Fields;
+using FieldType = Quick.Fields.FieldType;
+
+namespace Quick.Blazor.MudBlazor.QuickFields;
+
+public partial class Controls : ComponentBase
+{
+    [Inject]
+    public IDialogService DialogService { get; set; }
+
+    public FieldForGet[] Fields { get; private set; }
+
+    [Parameter]
+    public Action<FieldForGet, FieldForGet[]> OnFieldChangedAction { get; set; }
+
+    public void SetFields(FieldForGet[] fields)
+    {
+        travelFields(fields, field =>
+        {
+            if (field.PostOnChanged.HasValue && field.PostOnChanged.Value)
+                field.PropertyChanged -= OnFieldValueChanged;
+        });
+        Fields = fields;
+        travelFields(Fields, field =>
+        {
+            switch (field.Type)
+            {
+                case FieldType.Toast:
+                case FieldType.MessageBox:
+                    Action okAction = null;
+                    Action cancelAction = null;
+                    var usePreTag = field.MessageBox_UsePreTag.HasValue && field.MessageBox_UsePreTag.Value;
+                    if (field.PostOnChanged.HasValue && field.PostOnChanged.Value)
+                    {
+                        var canCancel = field.MessageBox_CanCancel.HasValue && field.MessageBox_CanCancel.Value;
+                        okAction = () => field.Value = FieldForGet.MESSAGEBOX_VALUE_OK;
+                        if (canCancel)
+                            cancelAction = () => field.Value = FieldForGet.MESSAGEBOX_VALUE_CANCEL;
+                    }
+                    DialogService.ShowMessageBoxAsync(field.Name, field.Description)
+                        .ContinueWith(t =>
+                        {
+                            if (t.IsCanceled)
+                                return;
+                            var ret = t.Result;
+                            if (ret == null || !ret.Value)
+                                cancelAction?.Invoke();
+                            else
+                                okAction?.Invoke();
+                        });
+                    break;
+                case FieldType.Button:
+                case FieldType.Upload:
+                    field.PostOnChanged = true;
+                    break;
+            }
+            if (field.PostOnChanged.HasValue && field.PostOnChanged.Value)
+                field.PropertyChanged += OnFieldValueChanged;
+        });
+        InvokeAsync(StateHasChanged);
+    }
+
+    private void OnFieldValueChanged(object sender, PropertyChangedEventArgs e)
+    {
+        var field = (FieldForGet)sender;
+        OnFieldChanged(field);
+    }
+
+    private void OnFieldChanged(FieldForGet field)
+    {
+        OnFieldChangedAction?.Invoke(field, Fields);
+    }
+
+    public void Dispose()
+    {
+        if (Fields != null)
+            foreach (var field in Fields)
+                if (field.PostOnChanged.HasValue && field.PostOnChanged.Value)
+                    field.PropertyChanged -= OnFieldValueChanged;
+    }
+
+    private void travelFields(FieldForGet[] fields, Action<FieldForGet> action)
+    {
+        if (fields == null)
+            return;
+        foreach (var field in fields)
+        {
+            action.Invoke(field);
+            travelFields(field.Input_PrependChildren, action);
+            travelFields(field.Children, action);
+            travelFields(field.Input_AppendChildren, action);
+        }
+    }
+}
