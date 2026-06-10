9260207271003d162e8211aa942cccb9d1d4a406
张鹏 <scbeta@qq.com>
2026-06-10 17:29:47 +0800

    增加Button和InputCheckbox支持

--- Quick.Blazor.MudBlazor.QuickFields/ControlUtils.cs
+++ Quick.Blazor.MudBlazor.QuickFields/ControlUtils.cs
@@ -49,7 +49,7 @@ internal static class ControlUtils
         appendCommonClass(sb, field);
         return sb.ToString();
     }
-    
+
     public static bool GetInputReadOnly(FieldForGet field)
     {
         if (field.Input_ReadOnly == null)
